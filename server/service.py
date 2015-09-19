847e3429a89331ef71f261a5d5aa25d3e528f8f9
laiqu <laiquendir@gmail.com>
2015-09-19 20:40:14 +0200

    Added pLizon.

--- server/service.py
+++ server/service.py
@@ -2,20 +2,23 @@ from bankjson import JSONSerializable
 
 
 class Service(JSONSerializable):
-    def __init__(self, id_, name, tokens, estimate, description, price, estimated):
+    def __init__(self, id_, name, type_, tokens, estimate, description, phone, price):
         self.id_ = id_
         self.name = name
+        self.type = type_,
         self.tokens = tokens
         self.estimate = estimate
         self.description = description
         self.price = price
-        self.estimate = estimate
+        self.phone = phone
 
     def to_json(self):
         return {
             'id': self.id_,
             'name': self.name,
+            'type': self.type,
             'tokens': self.tokens,
             'estimate': self.estimate,
             'description': self.description,
+            'phone': self.phone,
         }
